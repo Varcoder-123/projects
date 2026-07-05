4ec44e9412f91d7ac9410e216ac663f233cc8855
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-07-05 18:11:46 +0530

    added the environments

--- Infrastrure/environment/prod/main.tf
+++ Infrastrure/environment/prod/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source              = "./modules/network"
+  source              = "../../modules/network"
   vnet_name           = var.vnet_name
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
