f9e69ae25e6eb3b3e545e128358f25e7b5067fad
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-07-05 20:41:03 +0530

    changed some of the files

--- Infrastrure/environment/dev/main.tf
+++ Infrastrure/environment/dev/main.tf
@@ -1,7 +1,7 @@
 module "network" {
   source              = "../../modules/network"
   vnet_name           = var.vnet_name
-  location            = var.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = var.address_space
   public_subnet_name  = var.public_subnet_name
@@ -10,4 +10,12 @@ module "network" {
   app_subnet_prefix   = var.app_subnet_prefix
   db_subnet_name      = var.db_subnet_name
   db_subnet_prefix    = var.db_subnet_prefix
+}
+
+module "security" {
+  source              = "../../modules/security"
+  app_nsg_name        = var.app_nsg_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  security_rules      = var.security_rules
 }
--- Infrastrure/environment/dev/provider.tf
+++ Infrastrure/environment/dev/provider.tf
@@ -4,5 +4,5 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "dev-rg"
-  location = var.location
+  location = "Japan East"
 }
