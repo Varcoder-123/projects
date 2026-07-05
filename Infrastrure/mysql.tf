16270f4cea9c0f2e8412215496e9f031037a54c1
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-07-05 11:05:34 +0530

    commented the mysql.tf nic.tf nsg.tf

--- Infrastrure/mysql.tf
+++ Infrastrure/mysql.tf
@@ -1,54 +1,54 @@
-resource "azurerm_private_dns_zone" "mysql_dns" {
-  name                = "mysql.private.mysql.database.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
-}
+# resource "azurerm_private_dns_zone" "mysql_dns" {
+#   name                = "mysql.private.mysql.database.azure.com"
+#   resource_group_name = azurerm_resource_group.rg.name
+# }
 
-resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link" {
-  name                  = "mysql-dns-link"
-  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
-  resource_group_name   = azurerm_resource_group.rg.name
+# resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link" {
+#   name                  = "mysql-dns-link"
+#   private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
+#   resource_group_name   = azurerm_resource_group.rg.name
 
-  virtual_network_id = azurerm_virtual_network.vnet.id
-}
+#   virtual_network_id = azurerm_virtual_network.vnet.id
+# }
 
-resource "azurerm_mysql_flexible_server" "mysql" {
-  name                = "vigneshmysqlserver123"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+# resource "azurerm_mysql_flexible_server" "mysql" {
+#   name                = "vigneshmysqlserver123"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  administrator_login    = "mysqladmin"
-  administrator_password = var.mysql_password
+#   administrator_login    = "mysqladmin"
+#   administrator_password = var.mysql_password
 
-  sku_name = "B_Standard_B1ms"
+#   sku_name = "B_Standard_B1ms"
 
-  version = "8.0.21"
+#   version = "8.0.21"
 
-  delegated_subnet_id = azurerm_subnet.db_subnet.id
+#   delegated_subnet_id = azurerm_subnet.db_subnet.id
 
-  private_dns_zone_id = azurerm_private_dns_zone.mysql_dns.id
+#   private_dns_zone_id = azurerm_private_dns_zone.mysql_dns.id
 
-  backup_retention_days = 7
+#   backup_retention_days = 7
 
-  geo_redundant_backup_enabled = false
+#   geo_redundant_backup_enabled = false
 
-  depends_on = [
-    azurerm_private_dns_zone_virtual_network_link.mysql_dns_link
-  ]
-}
+#   depends_on = [
+#     azurerm_private_dns_zone_virtual_network_link.mysql_dns_link
+#   ]
+# }
 
-resource "azurerm_mysql_flexible_database" "appdb" {
+# resource "azurerm_mysql_flexible_database" "appdb" {
 
-  name      = "ecommercedb"
+#   name      = "ecommercedb"
 
-  server_name = azurerm_mysql_flexible_server.mysql.name
+#   server_name = azurerm_mysql_flexible_server.mysql.name
 
-  resource_group_name = azurerm_resource_group.rg.name
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  charset   = "utf8"
+#   charset   = "utf8"
 
-  collation = "utf8_unicode_ci"
-}
+#   collation = "utf8_unicode_ci"
+# }
 
-output "mysql_fqdn" {
-  value = azurerm_mysql_flexible_server.mysql.fqdn
-}
+# output "mysql_fqdn" {
+#   value = azurerm_mysql_flexible_server.mysql.fqdn
+# }
--- Infrastrure/nic.tf
+++ Infrastrure/nic.tf
@@ -1,17 +1,17 @@
-resource "azurerm_network_interface" "app_nic" {
+# resource "azurerm_network_interface" "app_nic" {
 
-  name                = "app-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+#   name                = "app-nic"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  ip_configuration {
+#   ip_configuration {
 
-    name = "internal" #This is the label for the IP configuration
+#     name = "internal" #This is the label for the IP configuration
 
-    subnet_id = azurerm_subnet.app_subnet.id
+#     subnet_id = azurerm_subnet.app_subnet.id
 
-    private_ip_address_allocation = "Dynamic"
+#     private_ip_address_allocation = "Dynamic"
 
-    public_ip_address_id = azurerm_public_ip.vm_pip.id
-  }
-}
+#     public_ip_address_id = azurerm_public_ip.vm_pip.id
+#   }
+# }
