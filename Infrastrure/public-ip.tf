5e6a1a136afe8a15b7def8c8903e2f056344c44b
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-07-05 11:10:21 +0530

    commented public-ip.tf vm.tf

--- Infrastrure/public-ip.tf
+++ Infrastrure/public-ip.tf
@@ -1,10 +1,10 @@
-resource "azurerm_public_ip" "vm_pip" {
+# resource "azurerm_public_ip" "vm_pip" {
 
-  name                = "app-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+#   name                = "app-public-ip"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
 
-  allocation_method = "Static"
+#   allocation_method = "Static"
 
-  sku = "Standard"
-}
+#   sku = "Standard"
+# }
--- Infrastrure/vm.tf
+++ Infrastrure/vm.tf
@@ -1,31 +1,31 @@
-resource "azurerm_linux_virtual_machine" "vm" {
+# resource "azurerm_linux_virtual_machine" "vm" {
 
-  name                = "java-app-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+#   name                = "java-app-vm"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
 
-  size = "Standard_B2ls_v2"
+#   size = "Standard_B2ls_v2"
 
-  admin_username = "azureuser"
+#   admin_username = "azureuser"
   
-  admin_password = var.vm_password
+#   admin_password = var.vm_password
 
-  disable_password_authentication = false
+#   disable_password_authentication = false
 
-  network_interface_ids = [
-    azurerm_network_interface.app_nic.id
-  ]
+#   network_interface_ids = [
+#     azurerm_network_interface.app_nic.id
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"
-  }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Premium_LRS"
+#   }
 
-  source_image_reference {
+#   source_image_reference {
 
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+# }
