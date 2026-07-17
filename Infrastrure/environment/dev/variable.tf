1709d69714c310dd9614bb868c19147ef3247821
Vignesh Venkatachalam <vignesh.venkatachalam@cloud4c.com>
2026-07-17 23:43:35 +0530

    modified the variable in dev

--- Infrastrure/environment/dev/variable.tf
+++ Infrastrure/environment/dev/variable.tf
@@ -1,15 +1,3 @@
-variable "vm_password" {
-  description = "Admin password for Linux VM"
-  type        = string
-  sensitive   = true
-}
-
-variable "mysql_password" {
-  description = "Password for MySQL database"
-  type        = string
-  sensitive   = true
-}
-
 variable "vnet_name" {
   description = "Name of the virtual network"
   type        = string
